4152a6e7cfa0dedeb2e14d72ed1c4a5ba562b68b
Abedzadeh5928 <Abed59285928@gmail.com>
2016-03-16 20:47:51 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {119707014},--Sudo users
+    sudo_users = {200857709},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
